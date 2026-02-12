25793f6ec785f85327d609a4a0f9f49078b5a759
Oscar Bréhier <68658005+oscarbrehier@users.noreply.github.com>
2026-02-12 16:34:24 +0100

    establish connection with addr

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use clap::{Parser};
-use std::{fs, net::IpAddr, path::PathBuf, time::Duration};
+use anyhow::Context;
+use clap::{Parser, builder::Str};
+use ssh2::{Channel, Session};
+use std::{
+    fs, io::Read, net::{IpAddr, Ipv4Addr}, path::PathBuf, time::Duration
+};
 use tokio::{net::TcpStream, time::timeout};
 
 #[derive(Parser)]
@@ -12,6 +16,32 @@ struct Args {
     user: String,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum KeyFileError {
+    #[error("Key file not found at {0}")]
+    NotFound(PathBuf),
+    #[error("The path provided is a directory, not a file")]
+    IsDirectory,
+    #[error("Key file doesn't have the correct permissions to read: {0}")]
+    NoReadPermissions(String),
+    #[error("Could not read metadata: {0}")]
+    ParseMetadata(String),
+    #[error("File system error: {0}")]
+    Io(#[from] std::io::Error),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum SshError {
+    #[error("Failed to reach server at {0}: {1}")]
+    Network(IpAddr, std::io::Error),
+    #[error("SSH handshake failed: {0}")]
+    Protocol(#[from] ssh2::Error),
+    #[error("Authentication failed for user {0}. Check your private key")]
+    AuthFailed(String),
+    #[error("The background task panicked")]
+    ThreadError(#[from] tokio::task::JoinError),
+}
+
 async fn ping_server(addr: &IpAddr) -> bool {
     match timeout(Duration::from_secs(3), TcpStream::connect((*addr, 80))).await {
         Ok(Ok(_stream)) => true,
@@ -19,47 +49,71 @@ async fn ping_server(addr: &IpAddr) -> bool {
     }
 }
 
-#[derive(Debug)]
-enum KeyFileError {
-    NotFound,
-    IsDirectory,
-    NoReadPermissions(String),
-    ParseMetadata(String),
-    InvalidKey,
-}
-
 fn validate_key_file(path: &PathBuf) -> Result<(), KeyFileError> {
     if !path.exists() {
-        return Err(KeyFileError::NotFound);
+        return Err(KeyFileError::NotFound(path.clone()));
     }
 
-    if path.is_dir() {
+    let metadata = path.metadata()?;
+
+    if metadata.is_dir() {
         return Err(KeyFileError::IsDirectory);
     };
 
-    let contents = fs::read_to_string(path).map_err(|e| KeyFileError::NoReadPermissions(e.to_string()))?;
-
-    println!("{}", contents);
+    std::fs::read(path).map_err(|e| KeyFileError::NoReadPermissions(e.to_string()))?;
 
     Ok(())
 }
 
+async fn connect_ssh(addr: IpAddr, user: String, key_path: PathBuf) -> Result<Session, SshError> {
+    tokio::task::spawn_blocking(move || {
+        let stream =
+            std::net::TcpStream::connect((addr, 22)).map_err(|e| SshError::Network(addr, e))?;
+
+        let mut sess = Session::new()?;
+        sess.set_tcp_stream(stream);
+        sess.handshake()?;
+
+        sess.userauth_pubkey_file(&user, None, &key_path, None)?;
+
+        if !sess.authenticated() {
+            return Err(SshError::AuthFailed(user));
+        }
+
+        Ok(sess)
+    })
+    .await?
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    if ping_server(&args.ip).await {
-        println!("Server {} is reachable for user {}!", args.ip, args.user);
+    let addr = args.ip;
+    let key_path = args.key_path;
+    let user = args.user;
+
+    if ping_server(&addr).await {
+        println!("Server {} is reachable for user {}!", addr, user);
     } else {
         eprintln!("Could not reach server.");
     }
 
-    validate_key_file(&args.key_path).expect("err");
+    validate_key_file(&key_path)?;
+
+    let session = connect_ssh(addr.clone(), user.clone(), key_path.clone()).await?;
+
+    let mut channel = session.channel_session().unwrap();
+
+    channel.wait_close();
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Write;
     use std::net::Ipv4Addr;
 
     #[tokio::test]
@@ -67,5 +121,24 @@ mod tests {
         let ip = IpAddr::V4(Ipv4Addr::new(192, 0, 2, 1));
         let result = ping_server(&ip).await;
         assert_eq!(result, false, "Should return false");
+    }
+
+    #[test]
+    fn test_valid_file() {
+        let mut temp_file = tempfile::NamedTempFile::new().unwrap();
+        writeln!(temp_file, "some-data").unwrap();
+
+        let path = temp_file.path().to_path_buf();
+        let result = validate_key_file(&path);
+
+        assert!(result.is_ok(), "Should accept a valid file");
+    }
+
+    #[test]
+    fn test_missing_file_fails() {
+        let path = PathBuf::from("non_existing_file.txt");
+        let result = validate_key_file(&path);
+
+        assert!(matches!(result, Err(KeyFileError::NotFound(_))));
     }
 }
